bfcb2d804ca128ae34fdb3a0d4b39258c1127d4d
ukasyaaah <ukasyaaah@gmail.com>
2025-02-25 23:58:28 +0700

    Finished Widget Builder

--- lib/pages/builder.dart
+++ lib/pages/builder.dart
@@ -9,24 +9,28 @@ class BuilderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ListView.builder(
-                
-                itemBuilder:
-                    (context, index) => Container(
-                      width: 100,
-                      height: 100,
-                      color: Color.fromARGB(
-                        255,
-                        Random().nextInt(256),
-                        Random().nextInt(256),
-                        Random().nextInt(256),
-                      ),
-                    ),
-              ),
-            ],
+        appBar: AppBar(title: Text('Widget Builder')),
+        body: SizedBox(
+          child: GridView.builder(
+            padding: EdgeInsets.all(15),
+            itemCount: 9,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // Jumlah baris fix
+              mainAxisSpacing: 10, // Kebawah / Vertical
+              crossAxisSpacing: 10, // Kesamping / Horizontal
+            ),
+            itemBuilder:
+                (context, index) => Container(
+                  width: 100,
+                  height: 100,
+                  color: Color.fromARGB(
+                    255,
+                    Random().nextInt(256),
+                    Random().nextInt(256),
+                    Random().nextInt(256),
+                  ),
+                  child: Center(child: Text('Angka ke ${index + 1}')),
+                ),
           ),
         ),
       ),
