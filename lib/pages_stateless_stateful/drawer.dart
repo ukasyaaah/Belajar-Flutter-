3470f5c2c5fe0e69e24ae136f1a2293e7b931ee3
ukasyaaah <ukasyaaah@gmail.com>
2025-03-05 14:36:29 +0700

    Finished Drawer

--- lib/pages_stateless_stateful/drawer.dart
+++ lib/pages_stateless_stateful/drawer.dart
@@ -5,120 +5,55 @@ class DrawerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: DrawerTest());
+    return MaterialApp(home: Drawwer());
   }
 }
 
-class DrawerTest extends StatelessWidget {
-  const DrawerTest({super.key});
+class Drawwer extends StatelessWidget {
+  const Drawwer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Drawer')),
-
+      appBar: AppBar(),
       drawer: Drawer(
-        
         child: Column(
-          
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.yellow),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListTile(
-                    title: Text('ukasyaaah'),
-                    subtitle: Text('Ukhasyah Fauzan'),
-                    leading: CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.black,
-                    ),
-                  ),
-                ],
+            UserAccountsDrawerHeader(
+              accountName: Text('ukasyaaah'),
+              accountEmail: Text('Ukhasyah Fauzan'),
+
+              currentAccountPicture: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.black45,
               ),
             ),
             Expanded(
               child: ListView(
                 children: [
                   ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
+                    leading: Icon(Icons.splitscreen_rounded),
+                    title: Text('Popular'),
                     onTap: () => Navigator.pop(context),
                   ),
                   ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
+                    leading: Icon(Icons.search),
+                    title: Text('Search'),
                     onTap: () => Navigator.pop(context),
                   ),
                   ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
+                    leading: Icon(Icons.people),
+                    title: Text('Profile'),
                     onTap: () => Navigator.pop(context),
                   ),
                   ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
+                    leading: Icon(Icons.watch_later),
+                    title: Text('Watchlist'),
                     onTap: () => Navigator.pop(context),
                   ),
+                  Divider(),
                   ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
-                    title: Text('Close'),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.abc),
+                    leading: Icon(Icons.close),
                     title: Text('Close'),
                     onTap: () => Navigator.pop(context),
                   ),
