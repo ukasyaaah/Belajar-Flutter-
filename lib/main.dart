14d67103ffad4d66c13c68817ed9fccdfac5d468
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-04-13 17:30:14 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,4 @@
-import 'package:belajar_flutter/pages_http/future_builder.dart';
-import 'package:belajar_flutter/pages_http/http_delete.dart';
-import 'package:belajar_flutter/pages_http/http_get.dart';
-import 'package:belajar_flutter/pages_http/http_post.dart';
-import 'package:belajar_flutter/pages_http/http_put_patch.dart';
-import 'package:belajar_flutter/pages_http/latihan_materi/latihan_delete.dart';
-import 'package:belajar_flutter/pages_http/latihan_materi/latihan_get.dart';
-import 'package:belajar_flutter/pages_http/latihan_materi/latihan_get_lagi.dart';
-import 'package:belajar_flutter/pages_http/latihan_materi/latihan_post.dart';
-import 'package:belajar_flutter/pages_http/latihan_materi/latihan_post2.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/bottom_navbar.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/drawer.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/dropdown.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/latihan_materi/bottom_sheest.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/latihan_materi/dropdown_warna.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/navigation.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/navigation_bar.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/tab_bar.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/textfield.dart';
-import 'package:belajar_flutter/pages_stateless_stateful/bottom_sheet.dart';
+import 'package:belajar_flutter/pages_http/ftbuilder.dart';
 
 import 'package:flutter/material.dart';
 
@@ -30,6 +11,6 @@ class MyAmazingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FutureBuilderPage());
+    return MaterialApp(home: Ftbuilder());
   }
 }
---
+++ lib/pages_http/ftbuilder.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Ftbuilder extends StatefulWidget {
+  const Ftbuilder({super.key});
+
+  @override
+  State<Ftbuilder> createState() => _FtbuilderState();
+}
+
+class _FtbuilderState extends State<Ftbuilder> {
+  List<Map<String, dynamic>> allData = [];
+  Future getData() async {
+    try {
+      var response = await http.get(Uri.parse('https://reqres.in/api/users'));
+
+      List data = (jsonDecode(response.body) as Map<String, dynamic>)['data'];
+      data.forEach((element) {
+        allData.add(element);
+      });
+    } catch (e) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Murojaah Future Builder',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+      ),
+
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.builder(
+              itemCount: allData.length,
+              itemBuilder:
+                  (context, index) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        '${allData[index]['avatar']}',
+                      ),
+                    ),
+                    title: Text(
+                      '${allData[index]['first_name']} ${allData[index]['last_name']}',
+                    ),
+                    subtitle: Text('${allData[index]['email']}'),
+                  ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
