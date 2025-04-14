ab8eac74653abcc2c5c90b86a0bd9d2c977f6fb5
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-04-14 13:22:49 +0700

    Food List: FutureBuilder & HTTP GET Request Practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:belajar_flutter/pages_http/ftbuilder.dart';
 import 'package:belajar_flutter/pages_http/latihan_materi/latihan_ftbuilder.dart';
+import 'package:belajar_flutter/pages_http/latihan_materi/latihan_ftbuilder_lagi.dart';
 
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,10 @@ class MyAmazingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {'/FTBUILDER': (context) => const LatihanFtbuilder()},
+      routes: {
+        '/FTBUILDER': (context) => const LatihanFtbuilder(),
+        '/ftbuilderexam': (context) => const LatihanFtbuilderLagi(),
+      },
       home: Homie(),
     );
   }
@@ -26,24 +30,51 @@ class Homie extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.redAccent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButtonn(nav: '/FTBUILDER', teks: 'Cek User', color: Colors.red),
+            SizedBox(height: 20),
+            TextButtonn(
+              nav: '/ftbuilderexam',
+              teks: 'Cek Meals',
+              color: Colors.yellow,
             ),
-          ),
-          onPressed: () {
-            Navigator.of(context).pushNamed('/FTBUILDER');
-          },
-          child: Text(
-            'Cek User',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
-          ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextButtonn extends StatelessWidget {
+  final String nav;
+  final String teks;
+  final Color color;
+  const TextButtonn({
+    super.key,
+    required this.nav,
+    required this.teks,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: color,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      ),
+      onPressed: () {
+        Navigator.of(context).pushNamed(nav);
+      },
+      child: Text(
+        teks,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
         ),
       ),
     );
---
+++ lib/pages_http/latihan_materi/latihan_ftbuilder_lagi.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LatihanFtbuilderLagi extends StatefulWidget {
+  const LatihanFtbuilderLagi({super.key});
+
+  @override
+  State<LatihanFtbuilderLagi> createState() => _LatihanFtbuilderLagiState();
+}
+
+class _LatihanFtbuilderLagiState extends State<LatihanFtbuilderLagi> {
+  Future<List> getMeals() async {
+    var response = await http.get(
+      Uri.parse('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'),
+    );
+
+    if (response.statusCode == 200) {
+      List data = (jsonDecode(response.body) as Map<String, dynamic>)['meals'];
+      return data;
+    } else {
+      throw Exception('Fail Load Data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Meals')),
+      body: FutureBuilder(
+        future: getMeals(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Colors.yellow,
+                backgroundColor: Colors.yellow,
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Oopps!, Error : ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('Data Kosong'));
+          } else {
+            var allData = snapshot.data!;
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+              ),
+              itemCount: allData.length,
+              itemBuilder:
+                  (context, index) => Container(
+                    height: 2000,
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          '${allData[index]['strMealThumb']}',
+                          height: 150,
+                          width: 150,
+                        ),
+
+                        Text('${allData[index]['strMeal']}'),
+                      ],
+                    ),
+                  ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
