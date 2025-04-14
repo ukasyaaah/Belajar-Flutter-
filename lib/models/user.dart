a157e7f8f53a1625e8e9c74deec56c5f9674d9c3
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-04-15 00:26:32 +0700

    .

---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+class UserModel {
+  late String avatar;
+  late String name;
+  late String email;
+
+  UserModel({required this.avatar, required this.name, required this.email});
+}
--- lib/pages_http/latihan_materi/latihan_ftbuilder.dart
+++ lib/pages_http/latihan_materi/latihan_ftbuilder.dart
@@ -12,13 +12,15 @@ class LatihanFtbuilder extends StatefulWidget {
 class _LatihanFtbuilderState extends State<LatihanFtbuilder> {
   /// Function Future utk  HTTP Request (get data)
   Future<List> getData() async {
-    var response = await http.get(Uri.parse('https://reqres.in/api/users'));
+    final response = await http.get(
+      Uri.parse('https://reqres.in/api/users?page=2'),
+    );
 
     if (response.statusCode == 200) {
       List data = (jsonDecode(response.body) as Map<String, dynamic>)['data'];
       return data;
     } else {
-      throw Exception('Gagal Load Data');
+      throw Exception('Fail load data');
     }
   }
 
@@ -32,8 +34,7 @@ class _LatihanFtbuilderState extends State<LatihanFtbuilder> {
       ),
       body: FutureBuilder(
         future: getData(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          /// Kalo lagi nunggu ?
+        builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(
               child: CircularProgressIndicator(
@@ -41,18 +42,12 @@ class _LatihanFtbuilderState extends State<LatihanFtbuilder> {
                 backgroundColor: Colors.black,
               ),
             );
-
-            /// Kalo Data Kosong
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Waduh Error : ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return Center(child: Text('Data Kosong'));
-
-            /// Kalo Terjadi Error
-          } else if (snapshot.hasError == true) {
-            return Center(child: Text('Error : ${snapshot.error}'));
-
-            /// Kalo Berhasil
           } else {
-            List allData = snapshot.data!;
+            var allData = snapshot.data!;
             return ListView.builder(
               itemCount: allData.length,
               itemBuilder:
