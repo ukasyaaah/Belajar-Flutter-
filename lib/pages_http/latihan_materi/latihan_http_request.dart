461e9a787941eea8426016075d6dd11c90aa4e2b
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-05-06 20:11:04 +0700

    Coba gambar mood

--- lib/pages_http/latihan_materi/latihan_http_request.dart
+++ lib/pages_http/latihan_materi/latihan_http_request.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LatihanHttpRequest extends StatefulWidget {
   const LatihanHttpRequest({super.key});
@@ -8,8 +11,51 @@ class LatihanHttpRequest extends StatefulWidget {
 }
 
 class _LatihanHttpRequestState extends State<LatihanHttpRequest> {
+
+  Future getImage() async{
+    final response = await http.get(Uri.parse('https://mymood.mymood.my.id/api/avatars'));
+
+final data = jsonDecode(response.body);
+return data;
+
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FutureBuilder(
+        future:getImage() ,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+if(snapshot.connectionState == ConnectionState.waiting){
+  return Center(child: CircularProgressIndicator(),);
+}else if(snapshot.hasError){
+    return Center(child: Text('Error : ${snapshot.error}'),);
+}else{
+final data = snapshot.data!;
+return GridView.builder(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+       crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,), 
+  itemBuilder:(context, index) {
+    final dhata = data[index];
+    return Container(
+                  height: 2000,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        dhata['avatar_path'],
+                        height: 150,
+                        width: 150,
+                      ),
+                    ],
+                  ),
+                );
+},);
+}
+        },
+      ),
+    );
   }
 }
