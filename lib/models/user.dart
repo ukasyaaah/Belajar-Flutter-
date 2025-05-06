da909e9ac905dd0b67830be9a32e62e20798c183
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-05-06 13:03:52 +0700

    Rampung Model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,27 +1,31 @@
 class User {
+  final int id;
   final dynamic email;
   final String first_name;
   final String last_name;
   final String avatar;
 
   User({
+    required this.id,
     required this.email,
     required this.first_name,
     required this.last_name,
     required this.avatar,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
+  factory User.fromJson(Map<String, dynamic> e) {
     return User(
-      email: json['email'],
-      first_name: json['first_name'],
-      last_name: json['last_name'],
-      avatar: json['avatar'],
+      id: e['id'],
+      email: e['email'],
+      first_name: e['first_name'],
+      last_name: e['last_name'],
+      avatar: e['avatar'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'email': email,
       'first_name': first_name,
       'last_name': last_name,
--- lib/pages_http/latihan_materi/latihan_model_tfbuild.dart
+++ lib/pages_http/latihan_materi/latihan_model_tfbuild.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:belajar_flutter/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -11,58 +12,57 @@ class LatihanModelTfbuild extends StatefulWidget {
 }
 
 class _LatihanModelTfbuildState extends State<LatihanModelTfbuild> {
+  Future<List<User>> getUser() async {
+    final response = await http.get(
+      Uri.parse('https://reqres.in/api/users?page=2'),
+      headers: {'x-api-key': 'reqres-free-v1'},
+    );
+    print('Status Code: ${response.statusCode}');
+    print('Response Body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      List data = (jsonDecode(response.body) as Map<String, dynamic>)['data'];
+      return data.map((json) => User.fromJson(json)).toList();
+    } else {
+      throw Exception('Fail load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Future<List<User>> getUser() async {
-      final response = await http.get(
-        Uri.parse('https://reqres.in/api/users?page=1'),
-      );
-
-      if (response.statusCode == 200) {
-        List data = (jsonDecode(response.body) as Map<String, dynamic>)['data'];
-        return data.map((json) => User.fromJson(json)).toList();
-      } else {
-        throw Exception('Gagal Load Data');
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Model W FT Build'),
         backgroundColor: Colors.deepPurpleAccent,
+        title: Text('Model FT Builder'),
+        centerTitle: true,
       ),
-      body: FutureBuilder<List<User>>(
+      body: FutureBuilder(
         future: getUser(),
-        builder: (context, snapshot) {
-          /// Kalo loading
+
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
-
-          /// Kalo Error
           } else if (snapshot.hasError) {
-            return Center(child: Text('Error : ${snapshot.error}'));
-
-          /// Kalo data kosong
+            return Center(child: Text('${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return Center(child: Text('Data Kosong'));
           } else {
-            List<User> allData = snapshot.data!;
-          /// Kalo data kosong
-            if(allData.length == 0){
-              return Text('Gada data');
-            }
+            List<User> data = snapshot.data!;
             return ListView.builder(
-              itemCount: allData.length,
+              itemCount: data.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.deepPurpleAccent,
-                    backgroundImage: NetworkImage('${allData[index].avatar}'),
+                final user = data[index];
+                return Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(user.avatar),
+                      ),
+                      title: Text('${user.first_name} ${user.last_name}'),
+                      subtitle: Text(user.email),
+                    ),
                   ),
-                  title: Text(
-                    '${allData[index].first_name} ${allData[index].last_name}',
-                  ),
-                  subtitle: Text('${allData[index].email}'),
                 );
               },
             );
